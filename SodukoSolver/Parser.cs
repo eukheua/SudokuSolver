f4cbdac156525528b1a8ada35fd8639d3f79f964
SHAFIR-MAIN <shafir.ohad@gmail.com>
2022-12-22 21:03:58 +0200

    inserting the Parser class and the inputReciever

---
+++ SodukoSolver/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolver
+{
+    internal class Parser
+    {
+        public Parser() { } 
+        public int[,] parseString(string stringGrid,int size)
+        {
+            int[,] grid = new int[size, size];
+            for(int i=0;i<size;i++)
+            {
+                for (int j = 0;j < size; j++)
+                {
+                    grid[i, j] = Convert.ToInt32(stringGrid[i * size + j]-'0');
+                }
+            }
+            return grid;
+        }
+    }
+}
