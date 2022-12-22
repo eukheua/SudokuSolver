e91cc66b8153f0976d94ad5ab06c48d111dd0494
SHAFIR-MAIN <shafir.ohad@gmail.com>
2022-12-22 19:59:39 +0200

    Adding the Solver class and the Validator class

--- SodukoSolver/Program.cs
+++ SodukoSolver/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SodukoSolver 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] grid =
+            {
+                {3,7,0,5,9,0,0,0,1 },
+                {0,0,5,8,0,0,0,0,0 },
+                {0,1,0,3,0,6,4,0,0},
+                { 2,0,8,0,4,0,0,6,5},
+                {0,0,9,0,0,0,7,0,0 },
+                {5,4,0,0,6,0,8,0,2 },
+                {0,0,4,6,0,8,0,1,0 },
+                {0,0,0,0,0,7,9,0,0 },
+                {7,0,0,0,5,2,0,3,4 }
+            };
+            Grid g = new Grid(9);
+            g.updateGrid(grid);
+            Solver s = new Solver();
+            s.solve(g, 0, 0);
+            g.show();
+        }
+    }
+}
---
+++ SodukoSolver/Solver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolver
+{
+    internal class Solver
+    {
+        public Solver()
+        {
+
+        }
+        public bool solve(Grid grid,int row,int column)
+        {
+            if (row == grid.getGrid().GetLength(0))
+                return true;
+            else if (column == grid.getGrid().GetLength(0))
+            {
+                return solve(grid, row + 1, 0);
+            }
+            else if (grid.getCell(row, column) != 0)
+            {
+                return solve(grid, row, column + 1);
+            }
+            else
+            {
+                for (int number=0; number < grid.getGrid().GetLength(0) + 1; number++)
+                {
+                    if(Validator.validate(grid.getGrid(),row,column,number))
+                    {
+                        grid.setCell(number, row, column);
+                        if (solve(grid,row,column+1))
+                        {
+                            return true;
+                        }
+                        grid.setCell(0, row, column);
+                    }
+                }
+                return false;
+            }
+
+        }
+    }
+}
