7949ae069a5b3155ee12fe58772b8542698e980f
Ohad Shafir <117098182+eukheua@users.noreply.github.com>
2023-01-04 16:54:13 +0200

    Adding the The ColumnNode and DLXList classes
    ColumnNode class is responsible for representing the base node through which you can reach other nodes in diffrent columns
    DLX represents the quadrouple linked list that will be implemented as a spare matrix

---
+++ SodukoSolver/DataStructures/ColumnNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolver.DataStructures
+{
+    internal class ColumnNode:DancingNode
+    {
+        private int size;
+        private string name;
+        public ColumnNode(string n):base()
+        {
+            size = 0;
+            name = n;
+            this.setColumn(this);
+        }
+        public int getSize() { return size; }   
+        public string getName() { return name; } 
+        public void setSize(int size) { this.size = size;}
+        public void setName(string name) { this.name = name; }
+        public void cover()
+        {
+            removeLeftRight();
+
+            for (DancingNode i = this.getBottom(); i != this; i = i.getBottom())
+            {
+                for (DancingNode j = i.getRight(); j != i; j = j.getRight())
+                {
+                    j.removeTopBottom();
+                    j.getColumn().size--;
+                }
+            }
+        }
+
+        public void uncover()
+        {
+            for (DancingNode i = this.getTop(); i != this; i = i.getTop())
+            {
+                for (DancingNode j = i.getLeft(); j != i; j = j.getLeft())
+                {
+                    j.getColumn().size--;
+                    j.reinsertTopBottom();
+                }
+            }
+
+            reinsertLeftRight();
+        }
+    }
+}
---
+++ SodukoSolver/DataStructures/DancingNode.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukoSolver.DataStructures
+{
+    internal class DancingNode
+    {
+        private DancingNode left, right, top, bottom;
+        private ColumnNode? column;
+        public DancingNode()
+        {
+            left = right = top = bottom = this;
+            column = null;
+        }
+        public DancingNode(ColumnNode c)
+        {
+            left = right = top = bottom = this;
+            column = c;
+        }
+        public DancingNode getLeft() { return left; }
+        public DancingNode getRight() { return right; }
+        public DancingNode getTop() { return top; }
+        public DancingNode getBottom() { return bottom; }
+        public ColumnNode getColumn() { return column; }
+        public void setLeft(DancingNode dancingNode) { this.left = dancingNode; }
+        public void setRight(DancingNode dancingNode) { this.right = dancingNode; }
+        public void setTop(DancingNode dancingNode) { this.top = dancingNode; }
+        public void setBottom(DancingNode dancingNode) {this.bottom = dancingNode;}
+        public void setColumn(ColumnNode columnNode) { this.column = columnNode; }
+        public DancingNode linkDown(DancingNode node)
+        {
+            node.bottom = bottom;
+            node.bottom.top = node;
+            node.top = this;
+            bottom = node;
+            return node;
+        }
+
+        public DancingNode linkRight(DancingNode node)
+        {
+            node.right = right;
+            node.right.left = node;
+            node.left = this;
+            right = node;
+            return node;
+        }
+
+        public void removeLeftRight()
+        {
+            left.right = right;
+            right.left = left;
+        }
+
+        public void reinsertLeftRight()
+        {
+            left.right = this;
+            right.left = this;
+        }
+
+        public void removeTopBottom()
+        {
+            top.bottom = bottom;
+            bottom.top = top;
+        }
+
+        public void reinsertTopBottom()
+        {
+            top.bottom = this;
+            bottom.top = this;
+        }
+    }
+}
